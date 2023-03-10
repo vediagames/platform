e9716bd36e757c2a1b236467dc16b42909d7c0a6
Antonio Jelić <40711615+vediatoni@users.noreply.github.com>
2023-03-10 15:40:40 +0100

    Update session/domain/error.go

--- session/domain/error.go
+++ session/domain/error.go
@@ -9,7 +9,7 @@ func (e Error) Error() string {
 const (
 	ErrNoData            = Error("no data")
 	ErrEmptyID           = Error("empty ID")
-	ErrEmptyPageURL      = Error("empty pageURL")
+	ErrEmptyPageURL      = Error("empty page URL")
 	ErrInvalidCreatedAt  = Error("invalid created at")
 	ErrInvalidInsertedAt = Error("invalid inserted at")
 )
