88cbd803ce502465b3fff5be5d32542e5808470c
Antonio Jelic <antonio.jelic@vediagames.com>
2023-08-12 14:41:14 +0200

    Update game lists tables

--- db/schema/postgresql/20230812055054_create_lists.down.sql
+++ db/schema/postgresql/20230812055054_create_lists.down.sql
@@ -2,5 +2,8 @@ BEGIN;
 
 DROP TABLE public.list_games;
 DROP TABLE public.lists;
+DROP TABLE public.list_games_history;
+DROP TRIGGER list_games_after_change ON list_games;
+DROP FUNCTION list_games_change_trigger();
 
 COMMIT;
--- db/schema/postgresql/20230812055054_create_lists.up.sql
+++ db/schema/postgresql/20230812055054_create_lists.up.sql
@@ -11,9 +11,48 @@ CREATE TABLE public.list_games (
     game_slug TEXT REFERENCES public.games(slug),
     label TEXT,
     description TEXT,
-    inserted_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    activated_at TIMESTAMP WITH TIME ZONE,
-    deactivated_at TIMESTAMP WITH TIME ZONE
+    inserted_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE public.list_games_history (
+    history_id SERIAL PRIMARY KEY,
+    list_slug TEXT NOT NULL,
+    game_slug TEXT NOT NULL,
+    label TEXT,
+    description TEXT,
+    change_timestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    change_type TEXT NOT NULL CHECK (change_type IN ('INSERT', 'UPDATE', 'DELETE'))
+);
+
+CREATE OR REPLACE FUNCTION list_games_change_trigger()
+RETURNS TRIGGER AS
+$$
+DECLARE
+    row RECORD;
+BEGIN
+    IF (TG_OP = 'DELETE') THEN
+        row := OLD;
+    ELSE
+        row := NEW;
+    END IF;
+
+    INSERT INTO list_games_history(list_slug, game_slug, label, description, change_timestamp, change_type)
+    VALUES (
+        row.list_slug,
+        row.game_slug,
+        row.label,
+        row.description,
+        NOW(),
+        TG_OP
+    );
+
+    RETURN row;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER list_games_after_change
+AFTER INSERT OR UPDATE OR DELETE ON list_games
+FOR EACH ROW
+EXECUTE FUNCTION list_games_change_trigger();
 
 COMMIT;
